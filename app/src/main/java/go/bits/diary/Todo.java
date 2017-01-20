6b077e1640b70a538aadccc574dd35c26115b6df
tanmaydixit <tanmaydixit1102@gmail.com>
2017-01-20 22:51:59 +0530

    To Do List added .

--- app/src/main/java/go/bits/diary/Todo.java
+++ app/src/main/java/go/bits/diary/Todo.java
@@ -1,9 +1,13 @@
 package go.bits.diary;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,9 +23,18 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import go.bits.diary.db.TaskContract;
 import go.bits.diary.db.TaskDbHelper;
+
+import static android.content.Context.ALARM_SERVICE;
 
 public class Todo extends AppCompatActivity {
 
@@ -30,6 +43,11 @@ public class Todo extends AppCompatActivity {
     private TaskDbHelper mHelper;
     private ListView mTaskListView;
     private ArrayAdapter<String> mAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
     /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);*/
@@ -59,18 +77,30 @@ public class Todo extends AppCompatActivity {
 
         mTaskListView = (ListView) findViewById(R.id.list_todo);
 
-        mHelper = new go.bits.diary.db.TaskDbHelper(this);
+        mHelper = new TaskDbHelper(this);
         SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(go.bits.diary.db.TaskContract.TaskEntry.TABLE,
-                new String[]{go.bits.diary.db.TaskContract.TaskEntry._ID, go.bits.diary.db.TaskContract.TaskEntry.COL_TASK_TITLE},
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
                 null, null, null, null, null);
-        while(cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(go.bits.diary.db.TaskContract.TaskEntry.COL_TASK_TITLE);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
             Log.d(TAG, "Task: " + cursor.getString(idx));
         }
+        /*Intent myIntent = new Intent(this, Todo.class);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 04);
+        calendar.set(Calendar.MINUTE, 00);
+        calendar.set(Calendar.SECOND, 00);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 24 *100, pendingIntent);  //set repeating every 24 hours
+        */
         updateUI();//wierd
         cursor.close();
         db.close();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //
@@ -79,8 +109,7 @@ public class Todo extends AppCompatActivity {
     //setContentView(R.layout.activity_main);
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
 
@@ -88,10 +117,8 @@ public class Todo extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.action_add_task:
                 Log.d(TAG, "Add a new task");
                 final EditText taskEditText = new EditText(this);
@@ -107,8 +134,8 @@ public class Todo extends AppCompatActivity {
                                 String task = String.valueOf(taskEditText.getText());
                                 SQLiteDatabase db = mHelper.getWritableDatabase();
                                 ContentValues values = new ContentValues();
-                                values.put(go.bits.diary.db.TaskContract.TaskEntry.COL_TASK_TITLE, task);
-                                db.insertWithOnConflict(go.bits.diary.db.TaskContract.TaskEntry.TABLE,
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
                                         null,
                                         values,
                                         SQLiteDatabase.CONFLICT_REPLACE);
@@ -118,7 +145,6 @@ public class Todo extends AppCompatActivity {
                         })
                         .setNegativeButton("Cancel", null)
                         .create();
-
 
 
                 dialog.show();
@@ -134,11 +160,11 @@ public class Todo extends AppCompatActivity {
     private void updateUI() {
         ArrayList<String> taskList = new ArrayList<>();
         SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(go.bits.diary.db.TaskContract.TaskEntry.TABLE,
-                new String[]{go.bits.diary.db.TaskContract.TaskEntry._ID, go.bits.diary.db.TaskContract.TaskEntry.COL_TASK_TITLE},
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
                 null, null, null, null, null);
         while (cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(go.bits.diary.db.TaskContract.TaskEntry.COL_TASK_TITLE);
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
             taskList.add(cursor.getString(idx));
         }
 
@@ -167,15 +193,47 @@ public class Todo extends AppCompatActivity {
         TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
         String task = String.valueOf(taskTextView.getText());
         SQLiteDatabase db = mHelper.getWritableDatabase();
-        db.delete(go.bits.diary.db.TaskContract.TaskEntry.TABLE,
-                go.bits.diary.db.TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
+        db.delete(TaskContract.TaskEntry.TABLE,
+                TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
                 new String[]{task});
         db.close();
         updateUI();
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Todo Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
